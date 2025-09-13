8373ab3a7680ef7de3dd7f007f20e23f626f7b98
mm <mmmdev2003@gmail.com>
2025-09-13 19:10:56 +0300

    dev

--- internal/model/__init__.py
+++ internal/model/__init__.py
@@ -1,5 +1,4 @@
 from internal.model.sql_model import *
 from internal.model.general import *
 from internal.model.organization import *
-from internal.model.publication import *
 from internal.model.organization import *
