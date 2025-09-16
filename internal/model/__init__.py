9088d6a88943106c1ff9106eccacf9790d5ddb0d
mm <mmmdev2003@gmail.com>
2025-09-16 15:16:44 +0300

    dev

--- internal/model/__init__.py
+++ internal/model/__init__.py
@@ -1,3 +1,3 @@
 from internal.model.sql_model import *
-from internal.model.organization import *
+from internal.model.client.kontur_authorization import *
 from internal.model.organization import *
