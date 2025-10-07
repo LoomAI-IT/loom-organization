9ea2cf9860242ac24b00ceb89c2c02788dfa0454
mm <mmmdev2003@gmail.com>
2025-10-07 10:16:21 +0300

    clean log

--- internal/controller/http/handler/organization/handler.py
+++ internal/controller/http/handler/organization/handler.py
@@ -8,6 +8,7 @@ from internal.controller.http.handler.organization.model import (
     CreateOrganizationBody, UpdateOrganizationBody,
     TopUpBalanceBody, DebitBalanceBody
 )
+from pkg.log_wrapper import auto_log
 
 from pkg.trace_wrapper import traced_method
 
@@ -24,15 +25,12 @@ class OrganizationController(interface.IOrganizationController):
         self.organization_service = organization_service
         self.interserver_secret_key = interserver_secret_key
 
+    @auto_log()
     @traced_method()
     async def create_organization(self, body: CreateOrganizationBody) -> JSONResponse:
-        self.logger.info("Начало создания организации")
-
         organization_id = await self.organization_service.create_organization(
             name=body.name,
         )
-
-        self.logger.info("Организация успешно создана")
         return JSONResponse(
             status_code=201,
             content={
@@ -40,25 +38,19 @@ class OrganizationController(interface.IOrganizationController):
             }
         )
 
+    @auto_log()
     @traced_method()
     async def get_organization_by_id(self, request: Request, organization_id: int) -> JSONResponse:
-        self.logger.info("Начало получения организации по ID")
-
         organization = await self.organization_service.get_organization_by_id(organization_id)
-
-        self.logger.info("Организация успешно получена")
         return JSONResponse(
             status_code=200,
             content=organization.to_dict()
         )
 
+    @auto_log()
     @traced_method()
     async def get_all_organizations(self) -> JSONResponse:
-        self.logger.info("Начало получения всех организаций")
-
         organizations = await self.organization_service.get_all_organizations()
-
-        self.logger.info("Все организации успешно получены")
         return JSONResponse(
             status_code=200,
             content={
@@ -66,10 +58,9 @@ class OrganizationController(interface.IOrganizationController):
             }
         )
 
+    @auto_log()
     @traced_method()
     async def update_organization(self, request: Request, body: UpdateOrganizationBody) -> JSONResponse:
-        self.logger.info("Начало обновления организации")
-
         await self.organization_service.update_organization(
             organization_id=body.organization_id,
             name=body.name,
@@ -84,38 +75,31 @@ class OrganizationController(interface.IOrganizationController):
             additional_info=body.additional_info
         )
 
-        self.logger.info("Организация успешно обновлена")
         return JSONResponse(
             status_code=200,
             content={}
         )
 
+    @auto_log()
     @traced_method()
     async def delete_organization(self, organization_id: int) -> JSONResponse:
-        self.logger.info("Начало удаления организации")
-
         await self.organization_service.delete_organization(organization_id)
-
-        self.logger.info("Организация успешно удалена")
         return JSONResponse(
             status_code=200,
             content={"message": "Organization deleted successfully"}
         )
 
+    @auto_log()
     @traced_method()
     async def top_up_balance(self, body: TopUpBalanceBody) -> JSONResponse:
         if body.interserver_secret_key != self.interserver_secret_key:
             self.logger.warning("Неверный межсервисный ключ для пополнения баланса")
             raise HTTPException(status_code=403, detail="Invalid interserver secret key")
 
-        self.logger.info("Начало пополнения баланса")
-
         await self.organization_service.top_up_balance(
             organization_id=body.organization_id,
             amount_rub=Decimal(body.amount_rub)
         )
-
-        self.logger.info("Баланс успешно пополнен")
         return JSONResponse(
             status_code=200,
             content={
@@ -124,20 +108,17 @@ class OrganizationController(interface.IOrganizationController):
             }
         )
 
+    @auto_log()
     @traced_method()
     async def debit_balance(self, body: DebitBalanceBody) -> JSONResponse:
         if body.interserver_secret_key != self.interserver_secret_key:
             self.logger.warning("Неверный межсервисный ключ для списания баланса")
             raise HTTPException(status_code=403, detail="Invalid interserver secret key")
 
-        self.logger.info("Начало списания баланса")
-
         await self.organization_service.debit_balance(
             organization_id=body.organization_id,
             amount_rub=Decimal(body.amount_rub)
         )
-
-        self.logger.info("Баланс успешно списан")
 
         return JSONResponse(
             status_code=200,
